eea6d07b55b48aa35df4ff40d8cfd0554d79f048
Alexey Kononov <clear-m@ya.ru>
2025-10-22 16:13:33 +0300

    back to the latest with new specs

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,8 @@
  * Give the service worker access to Firebase Messaging.
  * Note that you can only use Firebase Messaging here, other Firebase libraries are not available in the service worker.
 */
-importScripts('https://www.gstatic.com/firebasejs/7.15.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.15.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/12.4.0/firebase-messaging.js');
 
 // Firebase confing
 const firebaseConfig = {
@@ -18,24 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-/*
- * Retrieve an instance of Firebase Messaging so that it can handle background messages.
-*/
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
-    // Customize notification here
-    const notificationTitle = payload.data.title;
-    const notificationOptions = {
-        body: payload.data.body,
-        icon: '',
-        image: ''
-    };
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
 
-    return self.registration.showNotification(
-        notificationTitle,
-        notificationOptions
-    );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
